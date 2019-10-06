d92973d8c45163a5986f1a3175128a53369e9f18
hyun dong han <guehgk456@gmail.com>
2019-10-06 19:53:36 +0900

    project start

---
+++ app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dnights.nine2onetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_time.text = ""
+    }
+}
