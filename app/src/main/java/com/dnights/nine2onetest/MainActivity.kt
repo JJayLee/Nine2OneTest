f3ab777e520fcd9037b54d656bc2f524eb7896bc
hyun dong han <guehgk456@gmail.com>
2019-10-06 21:56:32 +0900

    fix sleep time 500 -> 100

--- app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
+++ app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
                         setTimeTextColor(seconds)
                     }
 
-                    SystemClock.sleep(500)
+                    SystemClock.sleep(100)
                 }
             }
         })
