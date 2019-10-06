ff64e28abaa4a5457a25a32618ac78fbbfaaa1b0
hyun dong han <guehgk456@gmail.com>
2019-10-06 21:47:20 +0900

    add text color change

--- app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
+++ app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.dnights.nine2onetest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.SystemClock
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -14,38 +14,56 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     val timeThread by lazy {
-        TimeThread(tv_time)
+        Thread(Runnable {
+            run {
+                while(true){
+                    val now = System.currentTimeMillis()
+                    val date = Date(now)
+                    val sdf = SimpleDateFormat("HH:mm:ss")
+                    val getTime = sdf.format(date)
+
+                    Handler(Looper.getMainLooper()).post {
+                        tv_time.text = getTime
+
+                        val seconds = getTime.split(":")[2].toInt()
+
+                        if(isReverseMode){
+                            if(seconds % 2 ==0) {
+                                tv_time.setTextColor(Color.BLUE)
+                            }else{
+                                tv_time.setTextColor(Color.RED)
+                            }
+                        }else{
+                            if(seconds % 2 ==0) {
+                                tv_time.setTextColor(Color.RED)
+                            }else{
+                                tv_time.setTextColor(Color.BLUE)
+                            }
+                        }
+                    }
+
+                    SystemClock.sleep(500)
+                }
+            }
+        })
     }
+
+    var isReverseMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         timeThread.start()
+
+        button_change.setOnClickListener {
+            isReverseMode = !isReverseMode
+        }
+
     }
 
     override fun onDestroy() {
         timeThread.interrupt()
         super.onDestroy()
-    }
-
-
-    class TimeThread(private val textView: TextView) : Thread() {
-        override fun run() {
-            super.run()
-            while(true){
-                val now = System.currentTimeMillis()
-                val date = Date(now)
-                val sdf = SimpleDateFormat("HH:mm:ss")
-                val getTime = sdf.format(date)
-
-                Handler(Looper.getMainLooper()).post {
-                    textView.text = getTime
-                }
-
-                SystemClock.sleep(500)
-            }
-
-        }
     }
 
 }
