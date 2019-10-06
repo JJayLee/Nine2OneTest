f1e921be956e64c4f0cd4d91011c7bd6a25ed843
hyun dong han <guehgk456@gmail.com>
2019-10-06 20:13:18 +0900

    add TimeThread

--- app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
+++ app/src/main/java/com/dnights/nine2onetest/MainActivity.kt
@@ -2,14 +2,50 @@ package com.dnights.nine2onetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val timeThread by lazy {
+        TimeThread(tv_time)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        tv_time.text = ""
+        timeThread.start()
     }
+
+    override fun onDestroy() {
+        timeThread.interrupt()
+        super.onDestroy()
+    }
+
+
+    class TimeThread(private val textView: TextView) : Thread() {
+        override fun run() {
+            super.run()
+            while(true){
+                val now = System.currentTimeMillis()
+                val date = Date(now)
+                val sdf = SimpleDateFormat("HH:mm:ss")
+                val getTime = sdf.format(date)
+
+                Handler(Looper.getMainLooper()).post {
+                    textView.text = getTime
+                }
+
+                SystemClock.sleep(500)
+            }
+
+        }
+    }
+
 }
